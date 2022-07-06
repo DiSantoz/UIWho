223ea1cb741b28568847fa0b03825633d2a73b13
David Ferreira <dsferreira06@gmail.com>
2022-07-06 08:11:52 -0400

    create tooltip

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-import "./test.css";
+import Tooltip from "./ui/tooltips";
 
-console.log("tests");
+// create tooltip
+const tooltip = new Tooltip(document.querySelector(".tooltip"));
+
+tooltip.init();
