171e7525def1412eb9694077cf39535e29dd524a
David Ferreira <dsferreira06@gmail.com>
2022-07-06 08:23:45 -0400

    create dropdown class

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import Tooltip from "./ui/tooltips";
+import Dropdown from "./ui/dropdown";
 
 // create tooltip
 const tooltip = new Tooltip(document.querySelector(".tooltip"));
 
 tooltip.init();
+
+//  create a dropdown
+const dropdowns = document.querySelectorAll(".dropdown");
+
+dropdowns.forEach((dropdown) => {
+  const instance = new Dropdown(dropdown);
+  instance.init();
+});
