39c70e91cc2bba93147f37fb44b302498bdd9dac
David Ferreira <dsferreira06@gmail.com>
2022-07-06 08:40:55 -0400

    create tabs and style

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Tooltip from "./ui/tooltips";
 import Dropdown from "./ui/dropdown";
+import Tabs from "./ui/tabs";
 
 // create tooltip
 const tooltip = new Tooltip(document.querySelector(".tooltip"));
@@ -13,3 +14,8 @@ dropdowns.forEach((dropdown) => {
   const instance = new Dropdown(dropdown);
   instance.init();
 });
+
+// create tabs
+
+const tabs = new Tabs(document.querySelector(".tabs"));
+tabs.init();
