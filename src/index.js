83bfa1e9af1ca2189f4119d6ac5fba60a195c907
David Ferreira <dsferreira06@gmail.com>
2022-07-06 08:53:31 -0400

    created snackbar and styled

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import Tooltip from "./ui/tooltips";
 import Dropdown from "./ui/dropdown";
 import Tabs from "./ui/tabs";
+import Snackbar from "./ui/snackbar";
 
 // create tooltip
 const tooltip = new Tooltip(document.querySelector(".tooltip"));
@@ -19,3 +20,13 @@ dropdowns.forEach((dropdown) => {
 
 const tabs = new Tabs(document.querySelector(".tabs"));
 tabs.init();
+
+//  create snackbar
+
+const snackbar = new Snackbar();
+snackbar.init();
+
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+  snackbar.show("you clicked me :o");
+});
